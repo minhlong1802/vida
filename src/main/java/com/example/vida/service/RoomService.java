7e546ab9ce0d7dfd2ad7cb361b59e546b19e0988
ntanh41 <ntanh41@cmcglobal.vn>
2024-10-25 16:42:41 +0700

    Get List Room Feature

--- src/main/java/com/example/vida/service/RoomService.java
+++ src/main/java/com/example/vida/service/RoomService.java
@@ -1,11 +1,9 @@
 package com.example.vida.service;
 
-import com.example.vida.dto.CreateRoomDto;
-import com.example.vida.entity.Room;
+import com.example.vida.dto.request.RoomFilterRequest;
 
-import java.util.List;
+import java.util.Map;
 
 public interface RoomService {
-    List<Room> getAllRooms();
-    Room createRoom(CreateRoomDto room);
+    Map<String, Object> filterRooms(RoomFilterRequest request);
 }
