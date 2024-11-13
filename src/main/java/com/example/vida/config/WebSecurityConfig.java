f7e30f710cf5ebe614e464a50429ca9882182ea6
Long. Nguyen Minh - CMC Global DU3.1 <nmlong@cmcglobal.vn>
2024-11-13 15:37:26 +0700

    modify export users function

--- src/main/java/com/example/vida/config/WebSecurityConfig.java
+++ src/main/java/com/example/vida/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.vida.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -39,6 +40,7 @@ public class WebSecurityConfig  {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(eh -> eh
